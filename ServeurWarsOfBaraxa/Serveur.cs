e917c7ddc0add9fff842e27923ad4c8f25289591
Ekmort <ydmule@hotmail.com>
2014-10-08 16:59:43 -0400

    Push

--- ServeurWarsOfBaraxa/Serveur.cs
+++ ServeurWarsOfBaraxa/Serveur.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
 using warsofbaraxa;
-using WarsOfBaraxaBD;
 
 namespace ServeurWarsOfBaraxa
 {
     class Serveur
     {
+        static private OracleConnection conn;
+        static private String connexionChaine;
         static Socket sck;
         static Socket client1 = null;
         static Socket client2 = null;
@@ -22,32 +26,122 @@ namespace ServeurWarsOfBaraxa
             sck.Listen(1000);
 
             Console.WriteLine("En attente de connexion");
-
+            
             while (true)
             {
-                //if car si il se deconnecte avant de partir la partis il ne va pas aprtier la partis et revenir ici, donc le if permet de savoir quel client a été deconnect
-                if (client1 == null)
-                {
-                    client1 = sck.Accept();
-                    Console.WriteLine("Joueur1 connecté");
-                }
-                if (client2 == null)
-                {
-                    client2 = sck.Accept();
-                    Console.WriteLine("Joueur2 connecté");
-                }
-                if (client1 != null && client2 != null)
-                {
-                    //envoye au 2 clients que la parti est commencée
-                    sendClient(client1, "La partie est commencee");
-                    sendClient(client2, "La partie est commencee");
-                }
+
+                Connection();
+                Carte[] CarteJoueur = ListerDeckJoueur("ekmort", 1);
+                ////if car si il se deconnecte avant de partir la partis il ne va pas aprtier la partis et revenir ici, donc le if permet de savoir quel client a été deconnect
+                //if (client1 == null)
+                //{
+                //    client1 = sck.Accept();
+                //    Console.WriteLine("Joueur1 connecté");
+                //}
+                //if (client2 == null)
+                //{
+                //    client2 = sck.Accept();
+                //    Console.WriteLine("Joueur2 connecté");
+                //}
+                //if (client1 != null && client2 != null)
+                //{
+                //    //envoye au 2 clients que la parti est commencée
+                //    sendClient(client1, "La partie est commencee");
+                //    sendClient(client2, "La partie est commencee");
+                //}
             }
         }
 
-        private Carte[] sendClientDeckJoueur
+        static private void sendDeckJoueurClient()
         {
+            Connection();
+            Carte[] CarteJoueur = ListerDeckJoueur("ekmort",1);
+            Deck DeckJoueur = new Deck(CarteJoueur);
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("172.17.104.114"), 1234);
+            try
+            {
+                sck.Connect(localEndPoint);
+            }
+            catch
+            {
+                System.Console.Write("Erreur de connexion");
+            }
 
+            if(sck.Connected)
+            {
+                byte[] data;
+                BinaryFormatter b = new BinaryFormatter();
+                using (var stream = new MemoryStream())
+                {
+                    b.Serialize(stream, DeckJoueur);
+                    data = stream.ToArray();
+                }
+                sck.Send(data);
+            }
+        }
+
+        static public void Connection()
+        {
+            String serveur = "(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 172.17.104.127)"
+            + "(PORT = 1522)))(CONNECT_DATA =(SERVICE_NAME = WarsOfBaraxa)))";
+            connexionChaine = "data source=" + serveur + ";userid=WarsOfBaraxa;password=WarsOfBaraxa";
+            conn = new OracleConnection(connexionChaine);
+            conn.Open();
+        }
+
+        static public Carte[] ListerDeckJoueur(String NomJoueur, int NoDeck)
+        {
+            Carte[] CarteJoueur = null;
+            string sql = "SELECT NomCarte,TypeCarte,Habilete,Ble,Bois,Gem,C.NoCarte,NombreDeFois FROM CARTE C " +
+            "INNER JOIN DeckCarte CD ON C.NoCarte=CD.NoCarte " +
+            "INNER JOIN DECK D ON CD.NoDeck=D.NoDeck " +
+            "INNER JOIN DECKJOUEUR DJ ON D.NoDeck=DJ.NoDeck WHERE DJ.IdJoueur=" + NomJoueur + " AND DJ.NoDeck=" + NoDeck;
+            OracleCommand commandeOracle = new OracleCommand(sql, conn);
+            OracleDataReader dataReader = commandeOracle.ExecuteReader();
+
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    int i = 0;
+                    for (int j = 0; j < dataReader.GetInt32(7); ++j)
+                    {
+                        CarteJoueur[i] = new Carte(dataReader.GetString(0), dataReader.GetString(1), dataReader.GetString(2), dataReader.GetInt32(3), dataReader.GetInt32(4), dataReader.GetInt32(5));
+                        if (dataReader.GetString(1) == "Permanents")
+                        {
+                            string sqlPerm = "SELECT TypePerm,Attaque,Armure,Vie FROM Permanents WHERE NoCarte=" + dataReader.GetInt32(6);
+                            OracleCommand commandeOraclePerm = new OracleCommand(sqlPerm, conn);
+                            OracleDataReader dataReaderPerm = commandeOraclePerm.ExecuteReader();
+                            CarteJoueur[i].perm = new Permanent(dataReaderPerm.GetString(0), dataReaderPerm.GetInt32(1), dataReaderPerm.GetInt32(2), dataReaderPerm.GetInt32(3));
+                        }
+                        ++i;
+                    }
+                }
+            }
+            return CarteJoueur;
+        }
+        public bool estPresent(string nomAlias, string mdp)
+        {
+            string sql = "select * from joueur where IdJoueur='" + nomAlias + "' and Pword='" + mdp + "'";
+            OracleCommand orac = new OracleCommand(sql, conn);
+            OracleDataReader dataReader = orac.ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool estDejaPresent(string nomAlias)
+        {
+            string sql = "select * from joueur where IdJoueur='" + nomAlias;
+            OracleCommand orac = new OracleCommand(sql, conn);
+            OracleDataReader dataReader = orac.ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
